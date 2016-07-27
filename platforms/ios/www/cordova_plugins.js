295acb66cb192ceb128df654e311073c98e56d1a
semenovem <semenovem@gmail.com>
2016-07-28 00:31:49 +0300

    .

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-vibration": "2.1.1"
+}
+// BOTTOM OF METADATA
+});
